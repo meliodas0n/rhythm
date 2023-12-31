35584cbc79c1fcc765222a4b183a68f43bd1004e
meliodas0n <mrunalnarayana@gmail.com>
2024-01-01 02:46:56 +0530

    updated run script

--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
+sudo apt-get update && sudo apt full-upgrade -y
 sudo apt-get install python3-tk libportaudio2 libasound-dev ffmpeg
+python3 -m venv .venv
+source ./.venv/bin/activate
 pip install -r requirements.txt
