96574d12411059f66cbecb8d5fae51f9929cd906
meliodas0n <mrunalnarayana@gmail.com>
2024-01-01 13:52:04 +0530

    safeside code

--- main.py
+++ main.py
@@ -25,35 +25,16 @@ class App(ctk.CTk):
         self.title(f"{self.name}")
         self.geometry(f"{self.xsize}x{self.ysize}")
 
-        # self.protocol("WM_DELETE_WINDOW", self.on_close)
-
-    # def on_close(self):
-    #     self.closed_by_user = True
-    #     Utils.on_closing_app(app)
-    #
-    # def __exit__(self, exc_type=None, exc_value=None, traceback=None):
-    #     return self.closed_by_user
-
 
 class MusicPlayer:
     def __init__(self, app):
         self.app = app
-
-    # def __exit__(self):
-    #     return True
 
     def run(self):
         try:
             self.app.mainloop()
         except Exception as e:
             print(f"Unable to run MusicPlayer due to following \n{e}")
-
-    # def protocol(self, func):
-    #     try:
-    #         if func == "WM_DELETE_WINDOW":
-    #             Utils.on_closing_app(self)
-    #     except Exception as e:
-    #         raise Exception("Undefined Protocol").with_traceback(e.__traceback__)
 
 
 class Utils:
