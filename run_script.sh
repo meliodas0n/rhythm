27dd4d3dad62ac881de229a97baa575e280fe52e
meliodas0n <mrunalnarayana@gmail.com>
2025-05-31 20:48:36 +0530

    don't need this in nixos

--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-sudo apt-get update && sudo apt full-upgrade -y
-sudo apt-get install python3-tk libportaudio2 libasound-dev ffmpeg
 python3 -m venv .venv
 source ./.venv/bin/activate
 pip install -r requirements.txt
